8f6dc7bedf29080589f7381d3d8ad167b40975e1
Yannik <yannikweber84@gmail.com>
2026-05-27 16:50:46 +0200

    new stable bot

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 
+// EXPRESS SERVER (für Render 24/7)
 const app = express();
-app.get('/', (req, res) => res.send('Bot läuft!'));
-app.listen(3000);
 
+app.get('/', (req, res) => {
+    res.send('Bot läuft!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Webserver läuft');
+});
+
+// DISCORD CLIENT
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -12,23 +20,44 @@ const client = new Client({
     ]
 });
 
-const TOKEN = process.env.TOKEN;
-
+// BOT ONLINE
 client.once('ready', () => {
-    console.log(`Bot online als ${client.user.tag}`);
+    console.log(`✅ Bot online als ${client.user.tag}`);
 });
 
+// WENN JEMAND JOINT
 client.on('guildMemberAdd', async (member) => {
 
-    const role1 = member.guild.roles.cache.find(r => r.name === "➢ Mitglied");
-    const role2 = member.guild.roles.cache.find(r => r.name === "┏―――――💎Mitglied💎――――┛");
-
     try {
-        if (role1) await member.roles.add(role1);
-        if (role2) await member.roles.add(role2);
+
+        // Rolle 1
+        const role1 = member.guild.roles.cache.find(
+            r => r.name === "➢ Mitglied"
+        );
+
+        // Rolle 2
+        const role2 = member.guild.roles.cache.find(
+            r => r.name === "┏―――――💎Mitglied💎――――┛"
+        );
+
+        // Rollen geben
+        if (role1) {
+            await member.roles.add(role1);
+        }
+
+        if (role2) {
+            await member.roles.add(role2);
+        }
+
+        console.log(`${member.user.tag} hat Rollen bekommen`);
+
     } catch (err) {
+
+        console.log('Fehler beim Rollen geben:');
         console.log(err);
+
     }
 });
 
-client.login(TOKEN);
+// LOGIN
+client.login(process.env.TOKEN);
