bcfcb523d189d50d32fee3b70d565fc2b6c3325e
yannikweber84-cloud <yannikweber84@gmail.com>
2026-05-28 23:52:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot läuft!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🌐 Server läuft auf Port ${PORT}`);
+});
+;
+require("dotenv").config();
 ```js
 const {
     Client,
